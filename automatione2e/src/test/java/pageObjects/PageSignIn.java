5a0fd451f30d7f5394a1cd9f1b2a3905adb83dc0
Alexander Yirsa <v.yirsa@uniandes.edu.co>
2024-04-21 14:38:47 -0500

    Test E2E Login

--- automatione2e/src/test/java/pageObjects/PageSignIn.java
+++ automatione2e/src/test/java/pageObjects/PageSignIn.java
@@ -10,12 +10,40 @@ public class PageSignIn {
     @FindBy(xpath = "(//a[@class='nav-link'])[1]")
     WebElement tuEspacio;
 
+    @FindBy(id = "email")
+    WebElement emailDeportista;
+
+    @FindBy(id = "password")
+    WebElement passwordDeportista;
+
+    @FindBy(xpath = "(//button[@class='btn btn-warning'])[1]")
+    WebElement botonIngresar;
+
+    @FindBy(xpath = "(//button[@class='btn btn-warning'])[2]")
+    WebElement botonSalir;
+
     public PageSignIn(WebDriver driver) {   
         PageFactory.initElements(driver, this);
     }
 
     public void tuEspacioInicio(){
         tuEspacio.click();
+    }
+
+    public void emailDeportista(String usuario){
+        emailDeportista.sendKeys(usuario);
+    }
+
+    public void passwordDeportista(String contraseña){
+        passwordDeportista.sendKeys(contraseña);
+    }
+
+    public void botonIngresarDeportista(){
+        botonIngresar.click();
+    }
+
+    public void botonSalirDeportista(){
+        botonSalir.click();
     }
 
 }
--- automatione2e/src/test/java/pruebas/BaseTestSuite.java
+++ automatione2e/src/test/java/pruebas/BaseTestSuite.java
@@ -1,16 +1,15 @@
 package pruebas;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pageObjects.PageSignIn;
 
@@ -33,15 +32,33 @@ public class BaseTestSuite {
     }
 
     @Test
-    public void clickTuEspacio(){
+    public void clickTuEspacioIngresar() throws InterruptedException{
+        String textEsperado = "Hello, sa-front-web - home";
+        String textoActual = null;
         driver.get("https://d1jiuccttec78g.cloudfront.net/#/");
         tuEspacioInicial.tuEspacioInicio();
+        tuEspacioInicial.emailDeportista("yirzajes2@gmail.com");
+        tuEspacioInicial.passwordDeportista("Ratica98*");
+        tuEspacioInicial.botonIngresarDeportista();
+        textoActual = driver.findElement(By.xpath("//h1[contains(text(),'Hello')]")).getText();
+        assertEquals(textEsperado, textoActual);
+    }
+
+    @Test
+    public void clickTuEspacioSalir(){
+        String tituloEsperado = "SportApp";
+        String tituloActual = null;
+        driver.get("https://d1jiuccttec78g.cloudfront.net/#/");
+        tuEspacioInicial.tuEspacioInicio();
+        tuEspacioInicial.emailDeportista("yirzajes2@gmail.com");
+        tuEspacioInicial.passwordDeportista("Ratica98*");
+        tuEspacioInicial.botonSalirDeportista();
+        tituloActual = driver.getTitle();
+        assertEquals(tituloActual, tituloEsperado);
     }
 
     @AfterMethod
     public void cerrarNavegador() {
         driver.quit();
     }
-
-
 }
